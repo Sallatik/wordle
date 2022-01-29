642a3f311cc04ae9b5127065aae2dc61201107d8
Salim Lati <niktonick@gmail.com>
2022-01-29 18:36:56 +0100

    mvp

--- src/components/Game.js
+++ src/components/Game.js
@@ -7,10 +7,14 @@ export default () => {
   const [state, setState] = useState(defaultState);
   const keyPressHandler = (e) => setState(updateState(state, e.key));
   return (
-    <div className="game" tabIndex="0" onKeyDown={keyPressHandler}>
-      {state.tiles.map((row) => (
-        <Row tiles={row} />
-      ))}
+    <div>
+      {state.won && <h1>Congrats!</h1>}
+      {state.lost && <h1>Loser!</h1>}
+      <div className="game" tabIndex="0" onKeyDown={keyPressHandler}>
+        {state.tiles.map((row) => (
+          <Row tiles={row} />
+        ))}
+      </div>
     </div>
   );
 };
