6b99eb532912ca18655611064cfe701b0c67dcbe
Salim Lati <niktonick@gmail.com>
2022-02-23 18:44:56 +0100

    capture keyboard focus

--- src/components/KeyboardFocus.js
+++ src/components/KeyboardFocus.js
@@ -3,8 +3,14 @@ import { useRef, useEffect } from "react";
 const KeyboardFocus = ({ onKeyDown }) => {
   const ref = useRef(null);
   useEffect(() => ref.current.focus(), [ref]);
-
-  return <div ref={ref} tabIndex="-1" onKeyDown={onKeyDown} />;
+  return (
+    <div
+      ref={ref}
+      tabIndex="-1"
+      onKeyDown={onKeyDown}
+      onBlur={() => ref.current.focus()}
+    />
+  );
 };
 
 export default KeyboardFocus;
